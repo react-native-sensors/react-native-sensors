777f98529c1a2e8713d45343c52739b6ba52b5fc
ImAtome <julienlaaraj@hotmail.fr>
2017-03-26 11:18:34 -0400

    Fix export named module (as in examples) (#6)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import Sensors from './src/sensors';
 import decorator from './src/decorator';
 
+export const { Accelerometer, Gyroscope, Magnetometer } = {...Sensors};
+export {decorator};
+
 export default {
   ...Sensors,
   decorator,
