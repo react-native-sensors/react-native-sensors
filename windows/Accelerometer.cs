c729a27a9e20aea13f510eaca6e512d8167ecbac
Christophe Lemonnier <lemonnier.christophe@gmail.com>
2017-04-03 14:55:47 +0100

    added support for UWP (#9)
    * added support for UWP
    
    * removed comments, implemented stop, fix from gyroscope, fixed x,y,z in upperCase, final implementation of interval variable
    
    * removed useless file, fixed dependencies on csproj

---
+++ windows/Accelerometer.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using System;
+using Windows.Foundation;
+using Sensors = Windows.Devices.Sensors;
+
+namespace RNSensors
+{
+    public class Accelerometer : ReactContextNativeModuleBase, ILifecycleEventListener
+    {
+        private Sensors.Accelerometer _accelerometer;
+        private int interval;
+
+        public Accelerometer(ReactContext reactContext) : base(reactContext)
+        {
+            
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Accelerometer";
+            }
+        }
+
+        private void ReadingChanged(object sender, Sensors.AccelerometerReadingChangedEventArgs e)
+        {
+            Sensors.AccelerometerReading reading = e.Reading;
+
+            this.SendEvent("Accelerometer", new RNSensorsJsonObject
+            {
+                X = reading.AccelerationX,
+                Y = reading.AccelerationY,
+                Z = reading.AccelerationZ,
+                Timestamp = reading.Timestamp
+            }.ToJObject());
+        }
+
+        [ReactMethod]
+        public void setUpdateInterval(int newInterval)
+        {
+            this.interval = newInterval;
+            if (_accelerometer != null) _accelerometer.ReportInterval = (uint)this.interval;
+        }
+
+        [ReactMethod]
+        public void startUpdates()
+        {
+            if (_accelerometer == null)
+            {
+                _accelerometer = Sensors.Accelerometer.GetDefault();
+                if (_accelerometer == null) throw new Exception("No Accelerometer found");
+
+                this.setUpdateInterval(this.interval);
+            }
+            _accelerometer.ReadingChanged += new TypedEventHandler<Sensors.Accelerometer, Sensors.AccelerometerReadingChangedEventArgs>(ReadingChanged);
+        }
+
+        [ReactMethod]
+        public void stopUpdates()
+        {
+            _accelerometer.ReadingChanged -= new TypedEventHandler<Sensors.Accelerometer, Sensors.AccelerometerReadingChangedEventArgs>(ReadingChanged);
+        }
+
+        private void SendEvent(string eventName, JObject parameters)
+        {
+            Context.GetJavaScriptModule<RCTDeviceEventEmitter>().emit(eventName, parameters);
+        }
+
+
+        public void OnDestroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSuspend()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
