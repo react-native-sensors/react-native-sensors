3230f7b363e2dc4a35384c0bacaaf7efd5f8d18c
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-12-25 18:28:22 +0100

    add more tests

--- src/__tests__/sensors.js
+++ src/__tests__/sensors.js
@@ -2,12 +2,13 @@ jest.unmock('rxjs/Rx');
 function createSensorMock() {
   return {
     setUpdateInterval: jest.fn(),
-    startUpdates: jest.fn(() => Promise.resolve()),
+    startUpdates: jest.fn(),
     stopUpdates: jest.fn()
   };
 }
 const mockGyro = createSensorMock();
 const mockAcc = createSensorMock();
+const mockDeviceEvents = jest.fn();
 const mockSensors = {
   Gyroscope: mockGyro,
   Accelerometer: mockAcc
@@ -15,7 +16,7 @@ const mockSensors = {
 jest.mock('react-native', () => ({
   NativeModules: mockSensors,
   DeviceEventEmitter: ({
-    addListener: jest.fn()
+    addListener: mockDeviceEvents
   })
 }));
 
@@ -28,6 +29,7 @@ describe("sensors", () => {
     mockAcc.setUpdateInterval.mockReset();
     mockAcc.startUpdates.mockReset();
     mockAcc.stopUpdates.mockReset();
+    mockDeviceEvents.mockReset();
   });
 
   it("should be mocked", () => {
@@ -50,7 +52,7 @@ describe("sensors", () => {
   });
 
   ['Accelerometer', 'Gyroscope'].forEach(type => {
-    describe(`Basic Interface: ${type}`, () => {
+    describe(type, () => {
       const Sensor = RNSensors[type];
       const sensorMock = mockSensors[type];
 
@@ -68,12 +70,82 @@ describe("sensors", () => {
         expect(sensorMock.setUpdateInterval).toHaveBeenCalledWith(100);
       });
 
+      it('should pass the correct update interval', () => {
+        const instance = new Sensor({
+          updateInterval: 20,
+        });
+        // If no one subscribes the observable is not invoked
+        instance.subscribe(value => { });
+        expect(sensorMock.setUpdateInterval).toHaveBeenCalledWith(20);
+      });
+
       it('should stop a sensor on command', () => {
         const instance = new Sensor();
         instance.subscribe(value => { });
         instance.stop();
 
         expect(sensorMock.stopUpdates).toHaveBeenCalled();
+      });
+
+      describe('events', () => {
+        it('should register a device event callback', async () => {
+          const { DeviceEventEmitter } = require('react-native');
+          const startUpdatesPromise = Promise.resolve();
+          sensorMock.startUpdates.mockReturnValueOnce(startUpdatesPromise);
+
+          const instance = new Sensor();
+          instance.subscribe(value => { });
+          expect(sensorMock.startUpdates).toHaveBeenCalled();
+          await startUpdatesPromise;
+
+          expect(DeviceEventEmitter.addListener).toHaveBeenCalled();
+        });
+
+        it('should add a value to the observable if an event was triggered', async () => {
+          const { DeviceEventEmitter } = require('react-native');
+          const startUpdatesPromise = Promise.resolve();
+          sensorMock.startUpdates.mockReturnValueOnce(startUpdatesPromise);
+          const instance = new Sensor();
+          const data = {
+            x: 0,
+            y: 0,
+            z: 0,
+            timestamp: 1514196009536
+          };
+
+          let lastValue;
+          instance.subscribe(value => { lastValue = value });
+          await startUpdatesPromise;
+
+          expect(DeviceEventEmitter.addListener.mock.calls[0][0]).toBe(type);
+          const emitEvent = DeviceEventEmitter.addListener.mock.calls[0][1];
+          emitEvent(data);
+
+          expect(lastValue).toEqual(data);
+        });
+
+        it('should error the observer if an error occured during initialization', async () => {
+          const { DeviceEventEmitter } = require('react-native');
+          const startUpdatesPromise = Promise.reject(new Error('Init impossible'));
+          sensorMock.startUpdates.mockReturnValueOnce(startUpdatesPromise);
+          const instance = new Sensor();
+          const data = {
+            x: 0,
+            y: 0,
+            z: 0,
+            timestamp: 1514196009536
+          };
+
+          let lastValue, lastError;
+          try {
+            instance.subscribe(value => { lastValue = value }, error => { lastError = error });
+            await startUpdatesPromise;
+          } catch(e) {
+            // expected
+          }
+
+            expect(lastError).toMatchSnapshot();
+        });
       });
     });
   });
