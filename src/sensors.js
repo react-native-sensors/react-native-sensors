4bc5b1a52943477599f1ed8ab5f07a32412dcaf7
David <davidescalera@concierges.es>
2018-02-18 12:45:02 +0100

    Promise chaining avoid to return a Promise

--- src/sensors.js
+++ src/sensors.js
@@ -4,36 +4,34 @@ import RNSensors from './rnsensors'
 
 function createSensorMonitorCreator(sensorType) {
   function Creator(options = {}) {
-    return new Promise((resolve, reject) => {
-      return RNSensors.isAvailable(sensorType).then(() => {
-        const {
-          updateInterval = 100, // time in ms
-        } = (options || {});
+    return RNSensors.isAvailable(sensorType).then(() => {
+      const {
+        updateInterval = 100, // time in ms
+      } = (options || {});
 
-        let observer;
+      let observer;
 
-        // Instanciate observable
-        const observable = Rx.Observable.create(function (obs) {
-          observer = obs;
-          // Start the sensor manager
-          RNSensors.start(sensorType, updateInterval).then(() => {
-            DeviceEventEmitter.addListener(sensorType, function(data) {
-              observer.next(data);
-            });
-          }, (error) => {
-            observer.error(error)
+      // Instanciate observable
+      const observable = Rx.Observable.create(function (obs) {
+        observer = obs;
+        // Start the sensor manager
+        RNSensors.start(sensorType, updateInterval).then(() => {
+          DeviceEventEmitter.addListener(sensorType, function(data) {
+            observer.next(data);
           });
+        }, (error) => {
+          observer.error(error)
         });
-
-        // Stop the sensor manager
-        observable.stop = () => {
-          RNSensors.stop(sensorType);
-          observer.complete();
-        };
-        resolve(observable);
-      }).catch(error => {
-        reject(error);
       });
+
+      // Stop the sensor manager
+      observable.stop = () => {
+        RNSensors.stop(sensorType);
+        observer.complete();
+      };
+      return observable;
+    }).catch(error => {
+      return error;
     });
   }
 
@@ -43,7 +41,7 @@ function createSensorMonitorCreator(sensorType) {
 // TODO: lazily intialize them (maybe via getter)
 const Accelerometer = createSensorMonitorCreator('Accelerometer');
 const Gyroscope = createSensorMonitorCreator('Gyroscope');
-const Magnetometer = createSensorMonitorCreator('Magnetometer');
+// const Magnetometer = createSensorMonitorCreator('Magnetometer');
 
 export default {
   Accelerometer,
