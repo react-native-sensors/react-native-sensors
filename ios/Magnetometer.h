54147188684290128171b638fae385a95b6fce6f
Noane Dan <Noane.Dan@outlook.com>
2018-03-15 19:00:49 +0200

    IOS Magnetometer

---
+++ ios/Magnetometer.h
@@ -0,0 +1,19 @@
+// Inspired by https://github.com/pwmckenna/react-native-motion-manager
+
+#import <React/RCTBridgeModule.h>
+#import <CoreMotion/CoreMotion.h>
+
+@interface Magnetometer : NSObject <RCTBridgeModule> {
+    CMMotionManager *_motionManager;
+}
+
+- (void) isAvailable
+         resolver:(RCTPromiseResolveBlock) resolve
+         rejecter:(RCTPromiseRejectBlock) reject;
+- (void) setUpdateInterval:(double) interval;
+- (void) getUpdateInterval:(RCTResponseSenderBlock) cb;
+- (void) getData:(RCTResponseSenderBlock) cb;
+- (void) startUpdates;
+- (void) stopUpdates;
+
+@end 
