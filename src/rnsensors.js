6410c2a0be2fa113808b8e57efb3ac344bafe287
Noane Dan <Noane.Dan@outlook.com>
2018-03-16 11:32:31 +0200

    Check for Magnetometer availability

--- src/rnsensors.js
+++ src/rnsensors.js
@@ -1,11 +1,7 @@
 import { NativeModules, DeviceEventEmitter } from "react-native";
 const { Gyroscope: GyroNative, Accelerometer: AccNative, Magnetometer: MagnNative  } = NativeModules;
 
-if (!GyroNative && !AccNative) {
-	throw new Error("Native modules for sensors not available. Did react-native link run successfully?");
-}
-
-if (!GyroNative && !AccNative) {
+if (!GyroNative && !AccNative && !MagnNative) {
 	throw new Error(
 		"Native modules for sensors not available. Did react-native link run successfully?"
 	);
