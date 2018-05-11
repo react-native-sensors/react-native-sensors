1260e9ef6c5a23b482e57a4a6c931b4fe5ff5929
Daniel Schmidt <dschmidt@mesosphere.com>
2018-05-11 14:09:38 +0200

    add sensor data

--- examples/BasicAccelerometer/App.js
+++ examples/BasicAccelerometer/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import { Accelerometer } from "react-native-sensors";
 
 const Value = ({name, value}) => (
   <View style={styles.valueContainer}>
     <Text style={styles.valueName}>{name}:</Text>
-    <Text style={styles.valueValue}>{value}</Text>
+    <Text style={styles.valueValue}>{new String(value).substr(0, 8)}</Text>
   </View>
 )
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    new Accelerometer({
+      updateInterval: 400 // defaults to 100ms
+    })
+      .then(observable => {
+        observable.subscribe(({x,y,z}) => this.setState({x,y,z}));
+      })
+      .catch(error => {
+        console.log("The sensor is not available");
+      });
+      
+    this.state = {x: 0, y: 0, z: 0};
+  }
+  
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.headline}>
           Accelerometer values
         </Text>
-        <Value name="x" value="42" />
-        <Value name="y" value="23" />
-        <Value name="z" value="18" />
+        <Value name="x" value={this.state.x} />
+        <Value name="y" value={this.state.y} />
+        <Value name="z" value={this.state.z} />
       </View>
     );
   }
@@ -45,7 +61,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   valueValue: {
-    width: 50,
+    width: 200,
     fontSize: 20
   },
   valueName: {
