41887124920fef5570816f3bf980be1c383e3f6c
Daniel Schmidt <dschmidt@mesosphere.com>
2018-05-22 09:19:43 +0200

    refactor game components

---
+++ examples/KeepTheBallGame/GameOver.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "react-native";
+
+export default function GameOverScreen({ onNewGame }) {
+  return (
+    <Button onPress={onNewGame} title="You lost. Click here to start again!" />
+  );
+}
