3f1da2d3b9764ebfa632d933d80d436712ef4d6c
Daniel Schmidt <dschmidt@mesosphere.com>
2018-11-28 20:40:52 +0100

    refactor: api access

--- examples/BasicSensors/SensorView.js
+++ examples/BasicSensors/SensorView.js
@@ -10,6 +10,7 @@ const Value = ({ name, value }) => (
 );
 
 export default function(sensorName) {
+  console.log("Starting sensor", sensorName);
   const sensor$ = Sensors[sensorName];
 
   return class SensorView extends Component {
@@ -20,10 +21,10 @@ export default function(sensorName) {
     }
 
     componentWillMount() {
-      debugger;
-      const subscription = sensor$.subscribe(({ x, y, z }) =>
-        this.setState({ x, y, z })
-      );
+      const subscription = sensor$.subscribe(({ x, y, z }) => {
+        console.log("sensor fires");
+        this.setState({ x, y, z });
+      });
       this.setState({ subscription });
     }
 
