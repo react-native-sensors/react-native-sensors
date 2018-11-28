4a0d1b217952d9af04ad0bd1677707e71f91ad4c
Daniel Schmidt <dschmidt@mesosphere.com>
2018-11-28 23:44:03 +0100

    fix(barometer): finally export barometer

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ export { setUpdateInterval as setUpdateIntervalForType } from "./src/rnsensors";
 export const SensorTypes = {
   accelerometer: "accelerometer",
   gyroscope: "gyroscope",
-  magnetometer: "magnetometer"
+  magnetometer: "magnetometer",
+  barometer: "barometer"
 };
 
-export const { accelerometer, gyroscope, magnetometer } = sensors;
+export const { accelerometer, gyroscope, magnetometer, barometer } = sensors;
 export default sensors;
