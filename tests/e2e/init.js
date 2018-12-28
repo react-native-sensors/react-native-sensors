44019f0daf6c0b95d4b69b20ed598f49cb87aaf3
Daniel Banck <daniel@dbanck.de>
2018-12-28 17:17:49 +0100

    style: run prettier on tests

--- tests/e2e/init.js
+++ tests/e2e/init.js
@@ -1,22 +1,23 @@
-const Telnet = require('telnet-client');
-const adapter = require('detox/runners/jest/adapter');
-const config = require('../package.json').detox;
-const detox = require('detox');
-const fs = require('fs');
-const path = require('path');
+const Telnet = require("telnet-client");
+const adapter = require("detox/runners/jest/adapter");
+const config = require("../package.json").detox;
+const detox = require("detox");
+const fs = require("fs");
+const path = require("path");
 
 const telnetParams = {
-    host: '127.0.0.1',
-    port: 5554,
-    shellPrompt: 'OK\r',
-    timeout: 2500
+  host: "127.0.0.1",
+  port: 5554,
+  shellPrompt: "OK\r",
+  timeout: 2500
 };
 
-const tokenFilePath = path.join(process.env.HOME, '.emulator_console_auth_token');
-
-const getEmulatorAuthToken = () => (
-  fs.readFileSync(tokenFilePath, 'utf8')
+const tokenFilePath = path.join(
+  process.env.HOME,
+  ".emulator_console_auth_token"
 );
+
+const getEmulatorAuthToken = () => fs.readFileSync(tokenFilePath, "utf8");
 
 jest.setTimeout(120000);
 jasmine.getEnv().addReporter(adapter);
@@ -26,7 +27,7 @@ beforeAll(async () => {
 
   const connection = new Telnet();
   await connection.connect(telnetParams);
-  const token = getEmulatorAuthToken()
+  const token = getEmulatorAuthToken();
   await connection.exec(`auth ${token}`);
 
   global.connection = connection;
