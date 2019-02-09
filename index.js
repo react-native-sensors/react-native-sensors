bfaefbeef1e81cb1e84708828639640d7becf9a8
RWOverdijk <r.w.overdijk@gmail.com>
2019-02-09 10:18:10 +0100

    refactor(project): export isAvailable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import sensors from "./src/sensors";
-export { setUpdateInterval as setUpdateIntervalForType } from "./src/rnsensors";
+export { isAvailable, setUpdateInterval as setUpdateIntervalForType } from "./src/rnsensors";
 
 export const SensorTypes = {
   accelerometer: "accelerometer",
