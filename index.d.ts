76009ae9b13af6128f5f0096a88655fe8634f25e
Jose Grana <josegranafdez@gmail.com>
2019-06-19 10:06:46 +0200

    feat(typescript): fix 'setUpdateIntervalTypes' params, sensors return a proper observable instead of a custom object

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,17 @@
 declare module "react-native-sensors" {
-  export const SensorTypes: {
+  import { Observable } from "rxjs";
+
+  type Sensors = {
     accelerometer: "accelerometer";
     gyroscope: "gyroscope";
     magnetometer: "magnetometer";
     barometer: "barometer";
   };
 
+  export const SensorTypes: Sensors;
+
   export const setUpdateIntervalForType: (
-    type: "accelerometer" | "gyroscope" | "magnetometer" | "barometer",
+    type: keyof Sensors,
     updateInterval: number
   ) => void;
 
@@ -18,34 +22,25 @@ declare module "react-native-sensors" {
     timestamp: string;
   }
 
-  import { Observable, Unsubscribable } from "rxjs";
-  export { Unsubscribable };
-  export type RNSensorObservable = Observable<SensorData>;
-
-  export interface RNSensor {
-    subscribe: (
-      observer: ({ x, y, z, timestamp }: SensorData) => SensorData
-    ) => Unsubscribable;
+  interface BarometerData {
+    pressure: number;
   }
+
+  type SensorsBase = {
+    accelerometer: Observable<SensorData>;
+    gyroscope: Observable<SensorData>;
+    magnetometer: Observable<SensorData>;
+    barometer: Observable<BarometerData>;
+  };
 
   export const {
     accelerometer,
     gyroscope,
     magnetometer,
     barometer
-  }: {
-    accelerometer: RNSensor;
-    gyroscope: RNSensor;
-    magnetometer: RNSensor;
-    barometer: RNSensor;
-  };
+  }: SensorsBase;
 
-  const sensors: {
-    accelerometer: RNSensor;
-    gyroscope: RNSensor;
-    magnetometer: RNSensor;
-    barometer: RNSensor;
-  };
+  const sensors: SensorsBase;
 
   export default sensors;
 }
