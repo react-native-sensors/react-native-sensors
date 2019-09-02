1e8c2e9195cde8f16552ad9b1caeaeeb7ccdfe24
simon <simon@medotemic.se>
2019-09-02 12:12:25 +0200

    fix: imported NativeModules

--- src/sensors.js
+++ src/sensors.js
@@ -1,4 +1,4 @@
-import { NativeEventEmitter } from "react-native";
+import { NativeEventEmitter, NativeModules } from "react-native";
 import { Observable } from "rxjs";
 import { publish, refCount } from "rxjs/operators";
 import * as RNSensors from "./rnsensors";
