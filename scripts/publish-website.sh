3e0f62c8c4bd2fcb6e512e9398960b935e2e4724
Daniel Schmidt <danielmschmidt92@gmail.com>
2020-05-20 18:05:47 +0200

    chore: ensure website deployment can only be run with valid env vars

--- scripts/publish-website.sh
+++ scripts/publish-website.sh
@@ -2,8 +2,25 @@
 
 set -ex
 
-git config user.email "$GIT_USER@users.noreply.github.com"
-git config user.name "$GIT_USER"
+if [ "$ENV" == "CI" ]; then
+    echo "This script will screw with your git config"
+    echo "if you really want to run it pass this env var: ENV=CI"
+    exit 1
+fi
+
+if [ -z "$GIT_USER" ]; then
+    echo "No GIT_USER provided"
+    exit 1
+fi
+
+if [ -z "$GIT_TOKEN" ]; then
+    echo "No GIT_TOKEN provided"
+    exit 1
+fi
+
+
+git config --global user.email "$GIT_USER@users.noreply.github.com"
+git config --global user.name "$GIT_USER"
 echo "machine github.com login $GIT_USER password $GIT_TOKEN" >~/.netrc
 
 cd website
