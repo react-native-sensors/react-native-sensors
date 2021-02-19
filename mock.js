2adf7dcefa3b1dde084fe6f1543a6760e3b0ff6a
Carlito <CharlesMangwa@users.noreply.github.com>
2021-02-19 14:42:30 +0100

    feat: add Jest mock implementation

---
+++ mock.js
@@ -0,0 +1,33 @@
+/**
+ * Library mock for test runners. e.g.:
+ *
+ * ```js
+ * jest.mock('react-native-sensors', () => require('react-native-sensors/mock'));
+ * ```
+ */
+
+const sensorMock = (observerValue) => ({
+  subscribe: (observer) => {
+    observer(observerValue || { x: 0, y: 0, z: 0, timestamp: 0 })
+    return ({ unsubscribe: jest.fn() })
+  },
+})
+
+const rnSensors = {
+  SensorTypes: {
+    accelerometer: 'accelerometer',
+    gyroscope: 'gyroscope',
+    magnetometer: 'magnetometer',
+    barometer: 'barometer',
+  },
+
+  accelerometer: sensorMock(),
+  gyroscope: sensorMock(),
+  magnetometer: sensorMock(),
+  barometer: sensorMock({ pressure: 0 }),
+
+  setLogLevelForType: jest.fn(),
+  setUpdateIntervalForType: jest.fn(),
+}
+
+module.exports = rnSensors
