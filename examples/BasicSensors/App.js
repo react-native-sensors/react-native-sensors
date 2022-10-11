80a32289f75d0ddc1535f51896e196e3f7894086
Kennedy Mumo <kemumo@microsoft.com>
2022-10-10 18:45:16 -0700

    Fix windows module reference

--- examples/BasicSensors/App.js
+++ examples/BasicSensors/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView } from "react-native";
 import SensorView from "./SensorView";
 
 export const App = () => {
