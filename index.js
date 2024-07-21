e062011cd620d386fe43d103adcfc1fdfb29ca62
fahadhaque007 <fahad@eqx.vc>
2024-07-22 11:18:02 +1200

    Update index.js
    Co-authored-by: Brendan O'Brien <titaniumcranium@icloud.com>

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ export const SensorTypes = {
   illuminance: "illuminance",
 };
 
-export const { accelerometer, gyroscope, magnetometer, barometer, orientation, gravity, luminance } = sensors;
+export const { accelerometer, gyroscope, magnetometer, barometer, orientation, gravity, illluminance } = sensors;
 export default sensors;
